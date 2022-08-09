4843c085f56211dc2702e188c2070f9176571c78
postpone-jiin <postpone.jin@gmail.com>
2022-08-09 21:44:19 +0900

    회원정보 관리 비즈니스 계층 구현

--- src/main/java/domain/MemberVO.java
+++ src/main/java/domain/MemberVO.java
@@ -17,4 +17,91 @@ public class MemberVO {
     private Integer birth;
     private Integer rank;
 
+    public Long getMemno() {
+        return memno;
+    }
+
+    public void setMemno(Long memno) {
+        this.memno = memno;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPhonenum() {
+        return phonenum;
+    }
+
+    public void setPhonenum(String phonenum) {
+        this.phonenum = phonenum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCompanyno() {
+        return companyno;
+    }
+
+    public void setCompanyno(String companyno) {
+        this.companyno = companyno;
+    }
+
+    public Integer getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Integer birth) {
+        this.birth = birth;
+    }
+
+    public Integer getRank() {
+        return rank;
+    }
+
+    public void setRank(Integer rank) {
+        this.rank = rank;
+    }
 }
