55c32bf20a31a476538bea93df37a5ab680f77fb
sonhyoseon <gytjs7623@naver.com>
2022-08-09 17:19:54 +0900

    견적게시판 CRUD 테스트 코드 작성

--- src/test/java/BoardDAOTest.java
+++ src/test/java/BoardDAOTest.java
@@ -1,5 +1,4 @@
-package org.zerock.test;
-
+import java.util.Date;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -12,10 +11,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
-import org.zerock.domain.BoardVO;
-import org.zerock.domain.Criteria;
-import org.zerock.domain.SearchCriteria;
-import org.zerock.persistence.BoardDAO;
+import domain.BoardVO;
+import domain.Criteria;
+import domain.SearchCriteria;
+import persistence.BoardDAO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/**/*.xml" })
@@ -30,32 +29,51 @@ public class BoardDAOTest {
 	public void testCreate() throws Exception {
 
 		BoardVO board = new BoardVO();
-		board.setTitle("새로운 글을 넣습니다. ");
-		board.setContent("새로운 글을 넣습니다. ");
-		board.setWriter("user00");
+		board.setBoardNo(1);
+		board.setTitle("테스트코드 타이틀");
+		board.setCategory("테스트코드 카테고리");
+		board.setRegDate("2002-02-02");
+		board.setModifiedDate("2002-02-02");
+		board.setAddress("테스트코드 주소");
+		board.setArea(111);
+		board.setStartDate("2002-02-02");
+		board.setEndDate("2002-02-02");
+		board.setBudget("테스트코드 예산");
+		board.setPart("테스트코드 파트");
+		board.setRequire("테스트코드 요구사항");
+		board.setImage("테스트코드 이미지");
 		dao.create(board);
 	}
 
 	@Test
 	public void testRead() throws Exception {
 
-		logger.info(dao.read(1).toString());
+		logger.info(dao.read(29).toString());
 	}
 
 	@Test
 	public void testUpdate() throws Exception {
 
 		BoardVO board = new BoardVO();
-		board.setBno(1);
-		board.setTitle("수정된 글입니다.");
-		board.setContent("수정 테스트 ");
+		board.setBoardNo(28);
+		board.setTitle("테스트코드 수정 타이틀");
+		board.setCategory("테스트코드 수정 카테고리");
+		board.setAddress("테스트코드 수정 주소");
+		board.setModifiedDate("2003-03-03");
+		board.setArea(111);
+		board.setStartDate("2002-02-02");
+		board.setEndDate("2002-02-02");
+		board.setBudget("테스트코드 수정 예산");
+		board.setPart("테스트코드 수정 파트");
+		board.setRequire("테스트코드 수정 요구사항");
+		board.setImage("테스트코드 수정 이미지");
 		dao.update(board);
 	}
 
 	@Test
 	public void testDelete() throws Exception {
 
-		dao.delete(1);
+		dao.delete(28);
 	}
 
 	@Test
@@ -65,72 +83,5 @@ public class BoardDAOTest {
 
 	}
 
-	@Test
-	public void testListPage() throws Exception {
-
-		int page = 3;
-
-		List<BoardVO> list = dao.listPage(page);
-
-		for (BoardVO boardVO : list) {
-			logger.info(boardVO.getBno() + ":" + boardVO.getTitle());
-		}
-	}
-
-	@Test
-	public void testListCriteria() throws Exception {
-
-		Criteria cri = new Criteria();
-		cri.setPage(2);
-		cri.setPerPageNum(20);
-
-		List<BoardVO> list = dao.listCriteria(cri);
-
-		for (BoardVO boardVO : list) {
-			logger.info(boardVO.getBno() + ":" + boardVO.getTitle());
-		}
-	}
-
-	@Test
-	public void testURI() throws Exception {
-
-		UriComponents uriComponents = UriComponentsBuilder.newInstance().path("/board/read").queryParam("bno", 12)
-				.queryParam("perPageNum", 20).build();
-
-		logger.info("/board/read?bno=12&perPageNum=20");
-		logger.info(uriComponents.toString());
-
-	}
-
-	@Test
-	public void testURI2() throws Exception {
-
-		UriComponents uriComponents = UriComponentsBuilder.newInstance().path("/{module}/{page}").queryParam("bno", 12)
-				.queryParam("perPageNum", 20).build().expand("board", "read").encode();
-
-		logger.info("/board/read?bno=12&perPageNum=20");
-		logger.info(uriComponents.toString());
-	}
-
-	@Test
-	public void testDynamic1() throws Exception {
-
-		SearchCriteria cri = new SearchCriteria();
-		cri.setPage(1);
-		cri.setKeyword("글");
-		cri.setSearchType("t");
-
-		logger.info("=====================================");
-
-		List<BoardVO> list = dao.listSearch(cri);
-
-		for (BoardVO boardVO : list) {
-			logger.info(boardVO.getBno() + ": " + boardVO.getTitle());
-		}
-
-		logger.info("=====================================");
-
-		logger.info("COUNT: " + dao.listSearchCount(cri));
-	}
 
 }
